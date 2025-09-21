12fdf0fc16bf7c87bb6ad508c1d1e6dd0dec5611
Up-to-code <78514942+Up-to-code@users.noreply.github.com>
2025-09-21 12:14:43 +0300

    Delete route.ts

--- src/app/api/upload-url/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const storageId = await convex.storage.generateUploadUrl();
-  return NextResponse.json(storageId);
-}
